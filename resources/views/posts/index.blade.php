f2f88e2309b6161a048c6887b7609a1b00e12e3d
Mika-Uemura <uemuramika2023.s@gmail.com>
2023-05-14 11:44:38 +0900

    写真以外できた

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,16 +3,17 @@
         　index
     </x-slot>
 
-        <h1>チーム開発会へようこそ！</h1>
-        <h2>投稿一覧画面</h2>
+        <h1>掲示板</h1>
         <a href='/posts/create'>新規投稿</a>
         <div>
             @foreach ($posts as $post)
                 <div style='border:solid 1px; margin-bottom: 10px;'>
-                    <p>
-                        タイトル：<a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-                    </p>
-                    <p>カテゴリー：<a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a></p>
+                    <p>イベントの日付：</p><input type="date">
+                    <p>タイトル：<a href="/posts/{{ $post->id }}">{{ $post->title }}</a></p>
+                    <p>作成者：{{ $user->name }}</p>
+                    {{--写真--}}
+                    {{--<img src="{{ asset('storage/二郎.jpg')}}">--}}
+                    <p>カテゴリー：{{ $post->category->name }}</a></p>
                     <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
                         @csrf
                         @method('DELETE')
